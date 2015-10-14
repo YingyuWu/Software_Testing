04a7a89bfd6e7c286fea47c2cdfab7ae70c5d574
YingyuWu <yywucs@gmail.com>
2015-10-13 21:55:57 -0400

    updated

--- superlist_current/functional_tests.py
+++ superlist_current/functional_tests.py
@@ -24,28 +24,32 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do',self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',header_text)
-		# She is invited to enter a to-do item straight awa
 		
-
+		# She is invited to enter a to-do item straight awa
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy peacock feathers')
-		inputbox.send_keys(Keys.ENTER)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
-		# She types "Buy peacock feathers" into a text box (Edith's hobby
-		# is tying fly-fishing lures)
 		#self.assertEqual(
 			#inputbox.get_attribute('placeholder'),
 			#'Enter a to-do item'
 		#)
+
+		
+		# She types "Buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list table
-		#inputbox.send_keys(Keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		#import time
 		#time.sleep(10)
+		#There is still a text box inviting her to add another item
+		#enter "Use peacock feathers to make a fly"
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		#the page updates again and now shows both items on the table
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		#table = self.browser.find_element_by_id('id_list_table')

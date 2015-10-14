10bbdbc24fa9cbee09f3a43144d5599220c3b6ae
YingyuWu <yywucs@gmail.com>
2015-10-13 21:29:30 -0400

    C5

--- superlist_current/functional_tests.py
+++ superlist_current/functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
+
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -20,20 +25,37 @@ class NewVisitorTest(unittest.TestCase):
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',header_text)
 		# She is invited to enter a to-do item straight awa
+		
+
+
 		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
-		self.assertEqual(
-			inputbox.get_attribute('placeholder'),
-			'Enter a to-do item'
-		)
-		inputbox.send_keys(Keys.ENTER)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+		#self.assertEqual(
+			#inputbox.get_attribute('placeholder'),
+			#'Enter a to-do item'
+		#)
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list table
+		#inputbox.send_keys(Keys.ENTER)
+		#import time
+		#time.sleep(10)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		#table = self.browser.find_element_by_id('id_list_table')
+		#rows = table.find_elements_by_tag_name('tr')
+		#self.assertTrue(
+			#any(row.text == '1: Buy peacock feathers' for row in rows),
+			#"New to-do item did not appear in table"
+		#)
+		#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		#self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 		self.fail('Finish the test!')
 
